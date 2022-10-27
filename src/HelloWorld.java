172fe47dc3197e4f702789ecc0539570b57d4c3c
shahargraiver <graiver1@mail.tau.ac.il>
2022-10-27 15:19:09 +0300

    practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("");
+		String str1 = args[0], str2 = args[1];
+		char chr = str1.charAt(0);
+		int offset = Integer.parseInt(args[1]);
+		int chrNum = chr;
+		int num1 = chrNum + offset;
+		char res1 = (char)num1;
+		System.out.println(res1);
 	}
 }
